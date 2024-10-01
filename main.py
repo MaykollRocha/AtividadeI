3a46165810cbabb82cfa1fa3a821d67041cc92ca
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 14:26:35 +0000

    New commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def AnaliseDeEntrada():
         numeros = np.array(stringNumeros.split(" "))
         st.markdown(f"""
                     A sequencia de números : {numeros}  
-                    Apresenta a média: {numeros.mean}
+                    Apresenta a média: {numeros.mean()}
                     
                     """)
 def TCRS_Calculo():
