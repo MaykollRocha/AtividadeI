8505315abd828802931ae98de27aee9d3c197c93
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 12:09:49 +0000

    New commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
 def calcIMC():
+    IMC = lambda peso,altura : peso/(altura*altura)
     st.title("Caculadora de IMC")
     st.latex(r"\text{IMC} = \frac{\text{Peso}}{\text{Altura}^2}")
     st.image("Imagens/IMCTable.png")
+    peso = st.number_input("Entrada de Peso: ",min_value=0.00)
+    altura = st.number_input("Entrada de altura: ",min_value=0.00)
+    if peso and altura:
+        st.text(f"Seu IMC é de {IMC(peso,altura)}.")
+    else:
+        st.text("Não foi dado um dos valores.")
 
 
 st.title("Atividade I - Streamlit")
