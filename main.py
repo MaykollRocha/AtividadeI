28277ab3838a94d53569edadd7f4d3c52693a98e
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 12:03:59 +0000

    New commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+def calcIMC():
+    st.title("Caculadora de IMC")
+    st.latex(r"\text{IMC} = \frac{\text{Peso}}{\text{Altura}^2}")
+    st.image("Imagens/IMCTable.png")
+
+
 st.title("Atividade I - Streamlit")
+
+calcIMC()
