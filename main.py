f724e2d2acc249cd8823bd18bdb64324b03c3c1d
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 14:48:05 +0000

    New commit

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def coracao_plot():
     x = 16*np.sin(t)**3
     y = 13*np.cos(t) - 5*np.cos(2*t) - 2*np.cos(3*t) - np.cos(4*t)
 
-    plt.plot(x,y)
+    plt.plot(x,y,color='black')
+    plt.fill(x,y,color='r')
     
+    plt.title("Coração")
     plt.axis("equal")
     st.pyplot(plt)
 
