80620ecfd9989fa54f5b8f1d92fe67efeb977aac
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 12:34:44 +0000

    New commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def cotacao_atual():
     st.title("Cotações")
     moeda1 = st.selectbox('Valor da Moeda1', [k for k in moedas],0)
     moeda2 = st.selectbox('Valor da Moeda2', [k for k in moedas],1)
-    cotacao = yf.Ticker(f'{moedas[moeda1]}{moedas[moeda2]}=X').info['bid']
+    cotacao = round(yf.Ticker(f'{moedas[moeda1]}{moedas[moeda2]}=X').info['bid'],2)
     st.text(f"De {moeda1} para {moeda2} será de {cotacao} {moedas[moeda2]}")
 
 
