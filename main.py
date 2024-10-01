56c5e518b0039659360f28b1705668ca535d7d0a
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-10-01 12:11:08 +0000

    New commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def calcIMC():
     peso = st.number_input("Entrada de Peso: ",min_value=0.00)
     altura = st.number_input("Entrada de altura: ",min_value=0.00)
     if peso and altura:
-        st.text(f"Seu IMC é de {IMC(peso,altura)}.")
+        st.text(f"Seu IMC é de {round(IMC(peso,altura),2)}.")
     else:
         st.text("Não foi dado um dos valores.")
 
